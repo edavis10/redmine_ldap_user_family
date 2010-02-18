1d8ec0ad90786bdd0f09641774d77f030c0353a5
Eric Davis <edavis@littlestreamsoftware.com>
2010-02-18 10:14:13 -0800

    [#3605] Added a setting for mapping the custom field.

--- init.rb
+++ init.rb
@@ -5,4 +5,10 @@ Redmine::Plugin.register :redmine_ldap_user_family do
   author 'Author name'
   description 'This is a plugin for Redmine'
   version '0.0.1'
+
+  settings({
+             :partial => 'settings/ldap_user_family',
+             :default => {
+               'family_custom_field' => nil
+             }})
 end
