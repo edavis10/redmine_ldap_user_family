ed84d23e0d66f59cccea91fd963c37bcae6c7b4c
Eric Davis <edavis@littlestreamsoftware.com>
2010-03-16 15:43:43 -0700

    [#3605] Refactoring and removing old methods.

--- lib/redmine_ldap_user_family/patches/user_patch.rb
+++ lib/redmine_ldap_user_family/patches/user_patch.rb
@@ -15,24 +15,6 @@ module RedmineLdapUserFamily
       end
       
       module ClassMethods
-
-        # * 6 letters of the student's last name
-        # * 1 letter of the student's first name
-        # * student number:
-        #  * for students: 3 numbers
-        #  * for parents: 1 hyphen and the last two numbers of the related student id
-        def parent_regexp
-          /\A[\w ]{6}\w{1}-[\w]{2}\z/i
-        end
-
-        def child_regexp
-          /\A[\w ]{6}\w{1}[\w]{3}\z/i
-        end
-
-        def parent_or_child_regexp
-          /\A[\w ]{6}\w{1}[\w|-][\w]{2}\z/i
-        end
-
         def create_new_user_from_ldap_with_family_id(family_id_match)
           AuthSourceLdap.all(:conditions => {:onthefly_register => true}).each do |ldap|
             ldap_user = ldap.find_user_by_family_id(family_id_match)
@@ -55,15 +37,11 @@ module RedmineLdapUserFamily
           user = try_to_login_without_auto_add_family(login, password)
           if user && user.parent?
             if user.child.blank?
-              family_id, last_two = *user.convert_to_child(user.get_my_family_value)
-
-              User.create_new_user_from_ldap_with_family_id(family_id)
+              User.create_new_user_from_ldap_with_family_id(user.get_my_family_value)
             end
           elsif user && user.child?
             if user.parent.blank?
-              family_id = user.convert_to_parent(user.get_my_family_value)
-
-              User.create_new_user_from_ldap_with_family_id(family_id)
+              User.create_new_user_from_ldap_with_family_id(user.get_my_family_value)
             end
           end
           
@@ -82,26 +60,12 @@ module RedmineLdapUserFamily
 
         def child
           return nil unless parent?
-
-          child_value = CustomValue.find(:first,
-                                         :conditions => ["value = :value AND customized_type = 'Principal' AND customized_id != :me",
-                                                         {
-                                                           :value => convert_to_child(get_my_family_value),
-                                                           :me => id
-                                                         }])
-          child_value.customized if child_value
+          find_family_record
         end
 
         def parent
           return nil unless child?
-
-          parent_value = CustomValue.find(:first,
-                                         :conditions => ["value = :value AND customized_type = 'Principal' AND customized_id != :me",
-                                                         {
-                                                           :value => convert_to_parent(get_my_family_value),
-                                                           :me => id
-                                                         }])
-          parent_value.customized if parent_value
+          find_family_record
         end
 
         def get_my_family_value
@@ -112,16 +76,19 @@ module RedmineLdapUserFamily
           end
         end
 
-        def convert_to_child(value)
-          value
-        end
-
-        def convert_to_parent(value)
-          value
-        end
-        
         private
 
+        def find_family_record
+          family_value = CustomValue.find(:first,
+                                          :conditions => ["value = :value AND customized_type = 'Principal' AND customized_id != :me",
+                                                          {
+                                                            :value => get_my_family_value,
+                                                            :me => id
+                                                          }])
+          family_value.customized if family_value
+
+        end
+        
         def is_parent_or_child?
           case
           when Setting.plugin_redmine_ldap_user_family["parent_group_id"] &&
