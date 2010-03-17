6837af19b577b810f55616d4198698925c881d2e
Eric Davis <edavis@littlestreamsoftware.com>
2010-03-16 17:28:45 -0700

    [#3605] Refactor: DRY up the internals

--- lib/redmine_ldap_user_family/patches/issue_patch.rb
+++ lib/redmine_ldap_user_family/patches/issue_patch.rb
@@ -6,24 +6,20 @@ module RedmineLdapUserFamily
         base.class_eval do
           def recipients_with_user_family_included
             r = recipients_without_user_family_included
-
+            mail = nil
+            
             if author.parent? || author.child?
               alternative_mail = Setting.plugin_redmine_ldap_user_family['parent_email_override_field']
 
               if alternative_mail.present?
-
                 custom_value = author.parent_or_child.custom_value_for(alternative_mail)
-                
-                if custom_value.value.present?
-                  r << custom_value.value
-                else
-                  r << author.parent_or_child.mail
-                end
-              else
-                r << author.parent_or_child.mail
+                mail = custom_value.value if custom_value.value.present?
               end
+
+              mail ||= author.parent_or_child.mail
             end
 
+            r << mail if mail.present?
             r.compact.uniq
           end
 
