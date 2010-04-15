5f16f7011f03cc87ee854bf852d93da7fcfb0929
Eric Davis <edavis@littlestreamsoftware.com>
2010-04-15 15:27:47 -0700

    [#3907] Show when an alternative email is used.

--- lib/redmine_ldap_user_family/hooks/user_hooks.rb
+++ lib/redmine_ldap_user_family/hooks/user_hooks.rb
@@ -4,10 +4,21 @@ module RedmineLdapUserFamily
       def view_account_left_bottom(context={})
         return '' unless context[:user]
 
-        display_family_relationship(context[:user])
+        return display_if_alternative_mail_is_used(context[:user]) +
+          display_family_relationship(context[:user])
       end
 
       private
+
+      def display_if_alternative_mail_is_used(user)
+        if alt_mail = user.alternative_mail
+          content_tag(:h3, l(:ldap_user_family_text_alternative_mail)) +
+            content_tag(:p, l(:ldap_user_family_text_using_alternative_mail, :mail => mail_to(alt_mail, nil, :encode => "javascript")))
+
+        else
+          ''
+        end
+      end
 
       def display_family_relationship(user)
         record = user.parent_or_child
